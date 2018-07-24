cde95103bbe8237300a1198c64f1ca6e0cb30964
ShiXiangRui <17600149312@163.com>
2018-07-24 17:41:58 +0800

    访问jsp

---
+++ ruilexiong_manager_web/src/main/java/com/ruilexiong/edu/manager/UserController.java
@@ -0,0 +1,29 @@
+/**
+ * FileName: controller
+ * Author:   asus
+ * Date:     2018/7/24 17:22
+ * Description: 后台管理模块的web入口
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.ruilexiong.edu.manager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈后台管理模块的web入口〉
+ *
+ * @author asus
+ * @create 2018/7/24
+ * @since 1.0.0
+ */
+@Controller
+public class UserController {
+    @RequestMapping("/user/login")
+    public String login(){
+        return "a";
+    }
+}
